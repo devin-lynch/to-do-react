0f11244b24e340830f7aab64560621d51aeae4ff
devin-lynch <devin.l.lynch@gmail.com>
2022-09-23 18:01:19 -0500

    Complete part 2

--- src/MyList.js
+++ src/MyList.js
@@ -5,18 +5,63 @@ import ListItem from './ListItem'
 
 
 class MyList extends Component {
+
+  state = {
+    taskArray: this.props.theList,
+    newItem: '' // the value of the form input
+  }
+
+  // sets array to be empty
+  clearList = () => {
+    console.log('THIS IS THE CONSOLE LOG', 'clearing list!')
+    this.setState({
+        taskArray: []
+      })
+  }
+
+  // handle input change events
+  handleChange = e => {
+    console.log('input was changed!')
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  // handle form submission events
+  addItem = e => {
+    e.preventDefault()
+    console.log('The form was submitted!')
+    this.setState(prevState => {
+      return {
+        taskArray: [...prevState.taskArray, prevState.newItem],
+        newItem: ''
+      }
+    })
+  }
+
+
   render() {
-    let todoItems = this.props.theList.map((item, i) => {
+    let todoItems = this.state.taskArray.map((item, i) => {
       return <ListItem
-              task={item} key={i}   
+              task={item} key={`task=${i}`}   
               />
     })
     return (
       <div>
         <h1>Things I should stop procrastinating:</h1>
+        <form onSubmit={this.addItem}>
+          <label htmlFor='newItem'>New Todo: </label>
+          <input type="text"
+          placeholder="Type an item here"
+          onChange={this.handleChange}
+          value={this.state.newItem}
+          />
+          <button type={"submit"}>Add it!</button>
+        </form>
         <ul>
          {todoItems}
         </ul>
+        <button onClick={this.clearList}>Finished the list!</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import MyList from './MyList';
 import './index.css';
 
 const toDos = ['Listen to Hop Along', 'Drink fresh lemonade', 'Eat bacon and egg tacos']
 
-ReactDOM.render(
-  <MyList
-  theList={toDos}
-  />,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <MyList
+//   theList={toDos}
+//   />,
+//   document.getElementById('root')
+// )
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MyList theList={toDos} />
+  </React.StrictMode>
+)
 
 
 // If you want to start measuring performance in your app, pass a function
