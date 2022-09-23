3510f196775bb7dcf19e4526c2ebb46612a2a9c5
devin-lynch <devin.l.lynch@gmail.com>
2022-09-22 19:28:53 -0500

    Adjust index.js

--- src/MyList.js
+++ src/MyList.js
@@ -5,7 +5,10 @@ class MyList extends Component {
   render() {
     return (
       <div>
-
+        <h1>Things I should stop procrastinating:</h1>
+        <ul>
+          <li></li>
+        </ul>
       </div>
     )
   }
